90ccabf872f1a30dbebd51cd0b4c95aa349fdaeb
Taha <mohamedtahasta@gmail.com>
2023-11-02 10:14:39 +0100

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,10 @@
+// source https://www.youtube.com/watch?v=bI6e6qjJ8JQ
+//*****************************************
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		 new GameFrame();
+	}
+
+}
